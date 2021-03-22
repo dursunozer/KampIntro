675c06a2265cb1115ff14709780a622d270c0a77
Dursun Özer <Dursun669@outlook.com>
2021-03-22 18:16:02 +0300

    22 Şubat 2021

---
+++ Genericsİntro/Program.cs
@@ -0,0 +1,14 @@
+﻿using GenericsIntro;
+using System;
+
+namespace Genericsİntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MYList isimler = new MYList();
+
+        }
+    }
+}
